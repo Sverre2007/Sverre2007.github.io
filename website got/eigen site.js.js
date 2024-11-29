9304088297adfbe98a8c0cfdf59dfca2a3af9025
Sverre2007 <s.t.koornneef@gmail.com>
2024-11-29 17:41:06 +0100

    Made a simple website
    ghost of tsushima website

---
+++ website got/eigen site.js.js
@@ -0,0 +1,6 @@
+function searchFunction() {
+    var input, filter, h2Elements, h3Elements, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    h2Elements = document.getElementsByTagName("h2");
+    h3Elements = document.getElementsByTagName("h3");}
